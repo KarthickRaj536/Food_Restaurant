85d85e77bc5faabf69e99ade5a9b8e69c3150852
Karthick Raj <139567214+KarthickRaj536@users.noreply.github.com>
2024-08-05 22:37:10 +0530

    Code commit

---
+++ food/static/food/js/order.js
@@ -0,0 +1,64 @@
+var pcart = document.querySelector("#pcart")
+var ptotal = document.querySelector("#ptotal")
+
+function addPizza(pid){
+    pizzaId = '#piz' + pid;
+    var name = document.querySelector(pizzaId).innerHTML;
+    var radio = 'pizza' + pid;
+    var pri = document.getElementsByName(radio);
+    var size,price;
+    if (pri[0].checked){
+        price = pri[0].value;
+        size ='M';
+    }
+    else{
+        price = pri[1].value;
+        size = 'L';
+    }
+
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    var total = localStorage.getItem('total');
+    var cartSize = orders.length;
+    orders[cartSize] = [name,size,price];
+    localStorage.setItem('orders', JSON.stringify(orders));
+    total = Number(total) + Number(price);
+    localStorage.setItem('total', total);
+
+    var cart = document.querySelector('#cart');
+    cart.innerHTML = orders.length;
+
+    butto = '<div class="del" onclick="removePizza(' + cartSize + ')">x</div>';
+    ptotal.innerHTML = 'Total: ' + total + ' $';
+    pcart.innerHTML += '<li>' + name + ' ' + size + ': ' + price + ' $' + butto + '</li>';
+}
+
+function pshoppingcart() {
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    var total = localStorage.getItem('total');
+    var cartSize = orders.length;
+
+    pcart.innerHTML = '';
+    for (let i = 0; i < cartSize; i++) {
+        butto = '<div class="del" onclick="removePizza(' + i + ')">x</div>';
+        pcart.innerHTML += '<li>'+ orders[i][0] + ' ' + orders[i][1] + ': ' + orders[i][2] + ' $' + butto + '</li>';
+    }
+    ptotal.innerHTML = 'Total: ' + total + ' $';
+}
+
+
+function removePizza(n) {
+    var orders = JSON.parse(localStorage.getItem('orders'));
+    var total = localStorage.getItem('total');
+    total = Number(total) - Number(orders[n][2]);
+    orders.splice(n,1);
+    var cart = document.querySelector('#cart');
+    cart.innerHTML = orders.length;
+    
+    localStorage.setItem('orders', JSON.stringify(orders));
+    localStorage.setItem('total', total);
+
+    pshoppingcart();
+}
+
+pshoppingcart();
+
